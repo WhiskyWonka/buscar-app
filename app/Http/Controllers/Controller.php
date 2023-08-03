000323d01a9e31ac215a3e58e9bc1498329710b4
Richard Grinberg <rggrinberg@gmail.com>
2023-08-03 23:37:45 +0000

    chore: chequeando php-cs-fixer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController{
-    use AuthorizesRequests, ValidatesRequests;
+/**
+este lo voy a comentar bien
+mal
+porque quiero
+que lo arregle
+php cs fixer
+ */
+class Controller extends BaseController
+{
+    use AuthorizesRequests;
+    use ValidatesRequests;
 }
